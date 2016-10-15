92ba95a917edf76f197a59fef42b9bc73befe003
Jimimimi <jimmy.messinis@gmail.com>
2016-10-15 15:56:35 +0300

    Add public assets

---
+++ public/javascripts/admin.js
@@ -0,0 +1,25 @@
+var socket = io('//localhost:3000');
+
+ion.sound({
+    sounds: [
+        {
+            name: "bell_ring"
+        },
+        {
+            name: "button_tiny",
+        }
+    ],
+    volume: 0.5,
+    path: "sounds/",
+    preload: true
+});
+socket.on('newTicket', function(data){
+  console.log('New ticket:', data);
+  ion.sound.play("bell_ring");
+});
+
+socket.on('updatedTicket', function(data){
+  console.log('Updated Ticket:', data);
+  ion.sound.play("button_tiny");
+
+});
