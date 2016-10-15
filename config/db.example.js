1076311b479cf366fcc827317a729dc2e914b56b
Jimimimi <jimmy.messinis@gmail.com>
2016-10-15 13:49:26 +0300

    Add Database example config

---
+++ config/db.example.js
@@ -0,0 +1,35 @@
+/*
+  Example database configuration file
+
+  Copy this file to db.js and edit accordingly
+*/
+
+var Sequelize = require('sequelize');
+
+var databases = {
+
+/* For example:
+  development: {
+    database: 'example',
+    user:     'example-user',
+    password: 'password',
+    options: {
+      logging: console.log
+    }
+*/
+  }
+}
+
+module.exports = new Sequelize(DBNAME,DBUSER,DBWORD,OPTIONS);
+
+function mountDB (env){
+  if (!databases[env]){
+    console.error('No database configuration for environment:', env);
+    return;
+  } else {
+    var db = databases[env];
+    return new Sequelize(db.database, db.user, db.password, db.options);
+  }
+}
+
+module.exports = mountDB;
